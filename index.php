83193a6899ef16202e9f293f43b60b8e58daf25f
avadnc <informaticalafell@gmail.com>
2018-10-31 22:58:19 -0600

    index

--- index.php
+++ index.php
@@ -7,50 +7,57 @@ require_once 'controlador/C_productos.php';
 require_once 'controlador/C_usuarios.php';
 require_once 'controlador/C_ventas.php';
 
-require_once 'modelo/M_categorias.php';
-require_once 'modelo/M_clientes.php';
-require_once 'modelo/M_productos.php';
-require_once 'modelo/M_usuarios.php';
-require_once 'modelo/M_ventas.php';
+require_once 'modelos/M_categorias.php';
+require_once 'modelos/M_clientes.php';
+require_once 'modelos/M_productos.php';
+require_once 'modelos/M_usuarios.php';
+require_once 'modelos/M_ventas.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+session_start();
 
 $plantilla = new C_principal;
 $plantilla->assign('titulo', 'titulo de prueba a cambiar');
 //parte del head
 $plantilla->display('modulos/cabecera.tpl');
+
+if (isset($_SESSION['iniciarSesion']) && $_SESSION['iniciarSesion'] == 'ok') {
 //seccion header
-$plantilla->display('modulos/header.tpl');
+    $plantilla->display('modulos/header.tpl');
 //seccion asode
-$plantilla->display('modulos/menu.tpl');
+    $plantilla->display('modulos/menu.tpl');
 //contenido
-if (isset($_GET['ruta'])) {
+    if (isset($_GET['ruta'])) {
 
-    if ($_GET['ruta'] == 'dashboard' ||
-        $_GET['ruta'] == 'usuarios' ||
-        $_GET['ruta'] == 'categorias' ||
-        $_GET['ruta'] == 'productos' ||
-        $_GET['ruta'] == 'clientes' ||
-        $_GET['ruta'] == 'crear-ventas' ||
-        $_GET['ruta'] == 'administrar-ventas' ||
-        $_GET['ruta'] == 'reporte-ventas') {
+        if ($_GET['ruta'] == 'dashboard' ||
+            $_GET['ruta'] == 'usuarios' ||
+            $_GET['ruta'] == 'categorias' ||
+            $_GET['ruta'] == 'productos' ||
+            $_GET['ruta'] == 'clientes' ||
+            $_GET['ruta'] == 'crear-ventas' ||
+            $_GET['ruta'] == 'administrar-ventas' ||
+            $_GET['ruta'] == 'reporte-ventas') {
 
-        $plantilla->display('modulos/' . $_GET['ruta'] . '.tpl');
+            $plantilla->display('modulos/' . $_GET['ruta'] . '.tpl');
 
-    } else if( $_GET['ruta'] == 'salir') {
-      $salir = new C_principal;
-      $salir->salir();
+        } else if ($_GET['ruta'] == 'salir') {
+            
+            C_principal::salir();
+
+        }else {
+            $plantilla->display('modulos/404.tpl');
+        }
+    } else {
+        $plantilla->display('modulos/dashboard.tpl');
     }
-    else {
-        $plantilla->display('modulos/404.tpl');
-    }
-} else {
-    $plantilla->display('modulos/dashboard.tpl');
-}
 
 
 //footer de la pagina
-$plantilla->display('modulos/footer.tpl');
+    $plantilla->display('modulos/footer.tpl');
+} else {
+    $plantilla->display('modulos/login.tpl');
+}
 //cierre del body
 $plantilla->display('modulos/piepagina.tpl');
